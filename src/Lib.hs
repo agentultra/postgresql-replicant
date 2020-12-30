938b902d592896573e6f2c59215e77ab0817b874
James King <james@agentultra.com>
2020-12-29 20:46:29 -0500

    Add some message types
    Added message types for:
    
      - Begin
      - Commit
      - Relation (metadata)
      - Insert

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,11 @@ module Lib
     ( someFunc
     ) where
 
+import Database.PostgreSQL.LibPQ
+import Database.PostgreSQL.Replicant.Message
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+withConnection :: Connection -> (Maybe ReplicationMessage -> IO ()) -> IO ()
+withConnection = undefined
