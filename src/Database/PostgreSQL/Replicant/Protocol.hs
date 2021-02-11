77a057f6f514088f011f614802deaf4b02609369
James King <james@agentultra.com>
2021-02-11 12:00:24 -0500

    Add connection setup and initialize streaming
    This change will enable the library to connect to the server and use a
    streaming replication slot.
    
    The example program is being used for development and debugging
    purposes for now until I cut the first release.  After that it should
    be a basic test program for users to check that the library can
    connect to their server and receive messages, similar to
    `pg_receivelogical`.

---
+++ src/Database/PostgreSQL/Replicant/Protocol.hs
@@ -0,0 +1,92 @@
+module Database.PostgreSQL.Replicant.Protocol where
+
+import Control.Monad (forever)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
+import Database.PostgreSQL.LibPQ
+
+data IdentifySystem
+  = IdentifySystem
+  { identifySystemSytemId  :: ByteString
+  , identifySystemTimeline :: ByteString
+  , identifySystemLogPos   :: ByteString
+  , identifySystemDbName   :: Maybe ByteString
+  }
+  deriving (Eq, Show)
+
+identifySystemCommand :: ByteString
+identifySystemCommand = "IDENTIFY_SYSTEM"
+
+identifySystemSync :: Connection -> IO (Maybe IdentifySystem)
+identifySystemSync conn = do
+  result <- exec conn identifySystemCommand
+  case result of
+    Nothing -> pure Nothing
+    Just r  -> do
+      systemId <- getvalue' r (toRow 0) (toColumn 0)
+      timeline <- getvalue' r (toRow 0) (toColumn 1)
+      logpos   <- getvalue' r (toRow 0) (toColumn 2)
+      dbname   <- getvalue' r (toRow 0) (toColumn 3)
+      case (systemId, timeline, logpos, dbname) of
+        (Just s, Just t, Just l, d) -> pure $ Just (IdentifySystem s t l d)
+        _ -> pure Nothing
+
+data ReplicationSlot =
+  ReplicationSlot
+  { replicationSlotName            :: ByteString
+  , replicationSlotConsistentPoint :: ByteString
+  , replicationSlotSnapshotName    :: ByteString
+  , replicationSlotOutputPlugin    :: ByteString
+  }
+  deriving (Eq, Show)
+
+createReplicationSlotCommand :: ByteString -> ByteString
+createReplicationSlotCommand slotName =
+  B.intercalate " " ["CREATE_REPLICATION_SLOT", slotName, "LOGICAL test_decoding"]
+
+createReplicationSlotSync :: Connection -> ByteString -> IO (Maybe ReplicationSlot)
+createReplicationSlotSync conn slotName = do
+  result <- exec conn $ createReplicationSlotCommand slotName
+  case result of
+    Nothing -> pure Nothing
+    Just r  -> do
+      sName           <- getvalue' r (toRow 0) (toColumn 0)
+      consistentPoint <- getvalue' r (toRow 0) (toColumn 1)
+      snapshotName    <- getvalue' r (toRow 0) (toColumn 2)
+      outputPlugin    <- getvalue' r (toRow 0) (toColumn 3)
+      case (sName, consistentPoint, snapshotName, outputPlugin) of
+        (Just s, Just c, Just sn, Just op) ->
+          pure $ Just (ReplicationSlot s c sn op)
+        _ -> pure Nothing
+
+startReplicationCommand :: ByteString -> ByteString -> ByteString
+startReplicationCommand slotName systemLogPos =
+  B.intercalate " " ["START_REPLICATION SLOT", slotName, "LOGICAL", systemLogPos]
+
+startReplicationStream :: Connection -> ByteString -> ByteString -> IO ()
+startReplicationStream conn slotName systemLogPos = do
+  result <- exec conn $ startReplicationCommand slotName systemLogPos
+  case result of
+    Nothing -> putStrLn "Woopsie" >>= \_ -> pure ()
+    Just r  -> do
+      status <- resultStatus r
+      case status of
+        CopyBoth -> forever $ do
+          d <- getCopyData conn False
+          case d of
+            CopyOutRow row -> do
+              print row
+            CopyOutError -> do
+              err <- errorMessage conn
+              print err
+              pure ()
+            CopyOutDone -> do
+              putStrLn "We should be finished with copying?"
+              pure ()
+            _ -> do
+              putStrLn "Unknown problem with getCopyData"
+              pure ()
+        _        -> do
+          err <- errorMessage conn
+          print err
+          pure ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,64 @@
+{-# LANGUAGE RecordWildCards #-}
 module Lib
-    ( someFunc
+    ( withConnection
     ) where
 
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Monad
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
+import Data.Maybe (fromMaybe)
 import Database.PostgreSQL.LibPQ
-import Database.PostgreSQL.Replicant.Message
+import GHC.Event
+import System.Posix.Types
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Database.PostgreSQL.Replicant.Protocol
 
-withConnection :: Connection -> (Maybe ReplicationMessage -> IO ()) -> IO ()
-withConnection = undefined
+data PgSettings
+  = PgSettings
+  { pgUser     :: String
+  , pgDbName   :: String
+  , pgHost     :: String
+  , pgPort     :: String
+  , pgSlotName :: String
+  }
+  deriving (Eq, Show)
+
+pgConnectionString :: PgSettings -> ByteString
+pgConnectionString PgSettings {..} = B.intercalate " "
+  [ "user=" <> (B.pack pgUser)
+  , "dbname=" <> (B.pack pgDbName)
+  , "host=" <> (B.pack pgHost)
+  , "port=" <> (B.pack pgPort)
+  , "replication=database"
+  ]
+
+withConnection :: PgSettings -> IO ()
+withConnection settings = do
+  conn <- connectStart $ pgConnectionString settings
+  mFd <- socket conn
+  let sockFd = fromMaybe (error "Failed getting socket file descriptor") mFd
+  pollResult <- pollConnectStart conn sockFd
+  case pollResult of
+    PollingFailed -> error "Unable to connect to the database"
+    PollingOk -> do
+      putStrLn "Connection ready!"
+      (Just info) <- identifySystemSync conn
+      (Just repSlot) <- createReplicationSlotSync conn "hackday2_sub_1"
+      startReplicationStream conn (replicationSlotName repSlot) (identifySystemLogPos info)
+      pure ()
+
+  where
+    pollConnectStart :: Connection -> Fd -> IO PollingStatus
+    pollConnectStart conn fd = do
+      pollStatus <- connectPoll conn
+      case pollStatus of
+        PollingReading -> do
+          threadWaitRead fd
+          pollConnectStart conn fd
+        PollingWriting -> do
+          threadWaitWrite fd
+          pollConnectStart conn fd
+        PollingOk -> pure PollingOk
+        PollingFailed -> pure PollingFailed
