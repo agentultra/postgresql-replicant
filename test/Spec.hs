ce29d5488f1b9b6b8d35bc0cdaedd91cebdf1ced
James King <james@agentultra.com>
2021-02-25 20:17:12 -0500

    Remove the Serialize instance on ResponseExpectation
    This seems better than initializing a new bytestring, calling decode,
    etc.

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,26 +3,23 @@ import Test.Hspec
 import Data.Serialize
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
+import Data.Word
 import GHC.Int
 
 import Database.PostgreSQL.Replicant.Message
 
 examplePrimaryKeepAliveMessage :: ByteString
 examplePrimaryKeepAliveMessage
-  = B.concat [(B.pack [0x6B]), (encode @Int64 123), (encode @Int64 346), (encode ShouldRespond)]
+  = B.concat
+    [ (B.pack [0x6B])
+    , (encode @Int64 123)
+    , (encode @Int64 346)
+    , (encode @Word8 1)
+    ]
 
 main :: IO ()
 main = hspec $ do
   context "Message" $ do
-    describe "ResponseExpectation" $ do
-      it "should decode a ShouldRespond" $ do
-        (decode $ B.pack [0x1])
-          `shouldBe`
-          (Right ShouldRespond)
-      it "should decode DoNotRespond" $ do
-        (decode $ B.pack [0x0])
-          `shouldBe`
-          (Right DoNotRespond)
     describe "PrimaryKeepAlive" $ do
       it "should decode a valid primary keep alive message" $ do
         (decode $ examplePrimaryKeepAliveMessage)
