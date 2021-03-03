cf5f52ed0bf34ade5b9a12783895ec3e3cf5286e
James King <james@agentultra.com>
2021-03-02 21:46:48 -0500

    Remove unneeded message types
    These were from when I was reading the wrong thing in the PostgreSQL
    docs

--- src/Database/PostgreSQL/Replicant/Message.hs
+++ src/Database/PostgreSQL/Replicant/Message.hs
@@ -10,63 +10,6 @@ import GHC.Int
 
 import Database.PostgreSQL.Replicant.Serialize
 
-newtype Oid a = Oid Int
-  deriving (Eq, Show)
-
-data Begin
-  = Begin
-  { _beginLsn        :: Int
-  , _beginCommitTime :: Integer
-  , _beginRemoteXid  :: Int
-  }
-  deriving (Eq, Show)
-
-data Commit
-  = Commit
-    { _commitLsn    :: Int
-    , _commitEndLsn :: Int
-    , _commitTime   :: Integer
-    }
-    deriving (Eq, Show)
-
-data Column = Column deriving (Eq, Show)
-
-data Origin
-  = Origin
-  { _originCommitLsn :: Int
-  , _originName      :: Text
-  }
-  deriving (Eq, Show)
-
-data Relation
-  = Relation
-  { _relationId              :: Oid Relation
-  , _relationNamespace       :: Text
-  , _relationName            :: Text
-  , _relationReplicaIdentity :: Int
-  , _relationColumns         :: [Column]
-  }
-  deriving (Eq, Show)
-
-data Insert
-  = Insert
-  { _insertRelationId :: Oid Relation
-  , _insertData       :: [Tuple]
-  }
-  deriving (Eq, Show)
-
-data Tuple = Tuple
-  deriving (Eq, Show)
-
-data ReplicationMessage
-  = BeginMessage Begin
-  | CommitMessage Commit
-  | OriginMessage Origin
-  | Relationmessage Relation
-  | InsertMessage Insert
-  | Unsupported
-  deriving (Eq, Show)
-
 data ResponseExpectation
   = ShouldRespond
   | DoNotRespond
