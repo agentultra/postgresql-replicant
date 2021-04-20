54eb45af1ecdd78c7f998bcc3a38eb86a53cc2f3
James King <james@agentultra.com>
2021-04-20 15:07:59 -0400

    Add LSN type to postgres utils
    Add an LSN type for use in status update messages and tracking stream
    progress.

--- src/Database/PostgreSQL/Replicant/PostgresUtils.hs
+++ src/Database/PostgreSQL/Replicant/PostgresUtils.hs
@@ -1,8 +1,33 @@
 module Database.PostgreSQL.Replicant.PostgresUtils where
 
 import Data.Fixed
+import Data.Serialize
 import Data.Time
 import GHC.Int
+
+data LSN = LSN
+  { filepart :: !Int32 -- ^ Filepart
+  , offset :: !Int32 -- ^ Offset
+  }
+  deriving (Show, Eq)
+
+instance Ord LSN where
+  compare (LSN l0 r0) (LSN l1 r1)
+    | l0 > l1 || (l0 == l1 && r0 > r1) = GT
+    | l0 < l1 || (l0 == l1 && r0 < r1) = LT
+    | otherwise = EQ
+
+instance Serialize LSN where
+  put (LSN filePart offSet) = do
+    putInt32be filePart
+    putWord8 0x2f -- '/' in ascii
+    putInt32be offSet
+
+  get = do
+    filePart <- getInt32be
+    skip 1
+    LSN filePart <$> getInt32be
+
 
 postgresEpoch :: IO Int64
 postgresEpoch = do
