049f1c17654de24ecd815f00d2b9f0efb073c1ae
James King <james@agentultra.com>
2021-04-27 20:47:23 -0400

    Remove extraneous `Maybe` from escape replication query functions

--- src/Database/PostgreSQL/Replicant/ReplicationSlot.hs
+++ src/Database/PostgreSQL/Replicant/ReplicationSlot.hs
@@ -36,14 +36,19 @@ parseSlotActive "t" = Active
 parseSlotActive "f" = Inactive
 parseSlotActive _   = Inactive
 
-createReplicationSlotCommand :: Connection -> ByteString -> IO (Maybe ByteString)
+createReplicationSlotCommand :: Connection -> ByteString -> IO ByteString
 createReplicationSlotCommand conn slotName = do
   escapedName <- escapeIdentifier conn slotName
   case escapedName of
-    Nothing -> pure Nothing
+    Nothing -> throwIO $ ReplicantException $ "Invalid slot name: " ++ show slotName
     Just escaped ->
-      pure $ Just $
-      B.intercalate "" ["CREATE_REPLICATION_SLOT", escaped, "LOGICAL wal2json"]
+      pure $
+      B.intercalate
+      ""
+      [ "CREATE_REPLICATION_SLOT"
+      , escaped
+      , "LOGICAL wal2json"
+      ]
 
 -- | Create a replication slot using synchronous query execution.
 -- @Nothing@ means the command was unsuccessful and the slot was not
@@ -51,32 +56,29 @@ createReplicationSlotCommand conn slotName = do
 createReplicationSlotSync :: Connection -> ByteString -> IO (Maybe ReplicationSlotInfo)
 createReplicationSlotSync conn slotName = do
   createReplicationSlotQuery <- createReplicationSlotCommand conn slotName
-  case createReplicationSlotQuery of
+  result <- exec conn createReplicationSlotQuery
+  case result of
     Nothing -> pure Nothing
-    Just query -> do
-      result <- exec conn query
-      case result of
-        Nothing -> pure Nothing
-        Just r  -> do
-          sName           <- getvalue' r (toRow 0) (toColumn 0)
-          consistentPoint <- getvalue' r (toRow 0) (toColumn 1)
-          outputPlugin    <- getvalue' r (toRow 0) (toColumn 3)
-          case (sName, consistentPoint, outputPlugin) of
-            (Just s, Just c, Just op) ->
-              case fromByteString c of
-                Left _ -> throwIO $ ReplicantException "createReplicationSlotSync: invalid LSN detected"
-                Right lsn -> pure $ Just (ReplicationSlotInfo s op Logical Active lsn)
-            _ -> do
-              err <- maybe "createReplicationSlotSync: unknown error" id <$> errorMessage conn
-              throwIO $ ReplicantException (B8.unpack err)
+    Just r  -> do
+      sName           <- getvalue' r (toRow 0) (toColumn 0)
+      consistentPoint <- getvalue' r (toRow 0) (toColumn 1)
+      outputPlugin    <- getvalue' r (toRow 0) (toColumn 3)
+      case (sName, consistentPoint, outputPlugin) of
+        (Just s, Just c, Just op) ->
+          case fromByteString c of
+            Left _ -> throwIO $ ReplicantException "createReplicationSlotSync: invalid LSN detected"
+            Right lsn -> pure $ Just (ReplicationSlotInfo s op Logical Active lsn)
+        _ -> do
+          err <- maybe "createReplicationSlotSync: unknown error" id <$> errorMessage conn
+          throwIO $ ReplicantException (B8.unpack err)
 
-getReplicationSlotInfoCommand :: Connection -> ByteString -> IO (Maybe ByteString)
+getReplicationSlotInfoCommand :: Connection -> ByteString -> IO ByteString
 getReplicationSlotInfoCommand conn slotName = do
   escapedName <- escapeStringConn conn slotName
   case escapedName of
-    Nothing -> pure Nothing
+    Nothing -> throwIO $ ReplicantException $ "Invalid slot name: " ++ show slotName
     Just escaped ->
-      pure $ Just $
+      pure $
       B.intercalate
       ""
       [ "select slot_name, plugin, slot_type, active, restart_lsn from pg_replication_slots where slot_name = '"
@@ -87,24 +89,21 @@ getReplicationSlotInfoCommand conn slotName = do
 getReplicationSlotSync :: Connection -> ByteString -> IO (Maybe ReplicationSlotInfo)
 getReplicationSlotSync conn slotName = do
   replicationSlotInfoQuery <- getReplicationSlotInfoCommand conn slotName
-  case replicationSlotInfoQuery of
+  result <- exec conn replicationSlotInfoQuery
+  case result of
     Nothing -> pure Nothing
-    Just query -> do
-      result <- exec conn query
-      case result of
-        Nothing -> pure Nothing
-        Just r  -> do
-          slotName    <- getvalue' r (toRow 0) (toColumn 0)
-          slotPlugin  <- getvalue' r (toRow 0) (toColumn 1)
-          slotType    <- getvalue' r (toRow 0) (toColumn 2)
-          slotActive  <- getvalue' r (toRow 0) (toColumn 3)
-          slotRestart <- getvalue' r (toRow 0) (toColumn 4)
-          case (slotName, slotPlugin, slotType, slotActive, slotRestart) of
-            (Just n, Just p, Just t, Just a, Just r) -> do
-              case fromByteString r of
-                Left _ -> pure Nothing -- TODO: this shouldn't happen...
-                Right lsn -> pure $ Just $ ReplicationSlotInfo n p (parseSlotType t) (parseSlotActive a) lsn
-            _ ->  pure Nothing
+    Just r  -> do
+      slotName    <- getvalue' r (toRow 0) (toColumn 0)
+      slotPlugin  <- getvalue' r (toRow 0) (toColumn 1)
+      slotType    <- getvalue' r (toRow 0) (toColumn 2)
+      slotActive  <- getvalue' r (toRow 0) (toColumn 3)
+      slotRestart <- getvalue' r (toRow 0) (toColumn 4)
+      case (slotName, slotPlugin, slotType, slotActive, slotRestart) of
+        (Just n, Just p, Just t, Just a, Just r) -> do
+          case fromByteString r of
+            Left _ -> pure Nothing -- TODO: this shouldn't happen...
+            Right lsn -> pure $ Just $ ReplicationSlotInfo n p (parseSlotType t) (parseSlotActive a) lsn
+        _ ->  pure Nothing
 
 -- | Create replication slot or retrieve the existing slot
 setupReplicationSlot :: Connection -> ByteString -> IO (Maybe ReplicationSlotInfo)
