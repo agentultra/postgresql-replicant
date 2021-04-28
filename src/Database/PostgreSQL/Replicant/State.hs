0096f74bc67d2b6673195bb9808c66a792cac0ad
James King <james@agentultra.com>
2021-04-27 21:21:44 -0400

    Add haddocks to State.hs

--- src/Database/PostgreSQL/Replicant/State.hs
+++ src/Database/PostgreSQL/Replicant/State.hs
@@ -1,3 +1,24 @@
+{-|
+Module      : Database.PostgreSQL.Replicant.State
+Description : Internal replication stream state
+Copyright   : (c) James King, 2021
+License     : GPL-3
+Maintainer  : james@agentultra.com
+Stability   : experimental
+Portability : POSIX
+
+This module has the types and functions for maintaining the client
+stream state.
+
+After initiating a replication stream the wal_sender process on the
+server may require clients to periodically send progress updates.  The
+wal_sender process uses those updates to maintain its internal view of
+the clients' state.
+
+This enables the server to report on things like replication lag and
+enables the client to disconnect and restart the stream where it left
+off.
+-}
 module Database.PostgreSQL.Replicant.State where
 
 import Control.Concurrent
